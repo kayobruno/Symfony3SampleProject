b034177307db2bf364b760f15c5ecdeb32c091ba
Bruno Lima <bruno@gsd.com.br>
2019-04-10 01:49:08 -0300

    Criar estrutura inicial de páginas e login
    As páginas Home e Login foram criadas e são exibidas de
    acordo com o login do usuário.

---
+++ src/AppBundle/Controller/HomepageController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/")
+ */
+class HomepageController extends Controller
+{
+    /**
+     * @Route("", name="homepage")
+     */
+    public function homepageAction()
+    {
+        if ($this->getUser()) {
+            return $this->render('homepage/index.html.twig');
+        }
+        return $this->render('homepage/login.html.twig');
+    }
+}
