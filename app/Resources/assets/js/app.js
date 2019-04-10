d5737aecb6708a7d3097efe8f2d5d9cf86ebe640
Bruno Lima <bruno@gsd.com.br>
2019-04-10 01:57:59 -0300

    Configurar Webpack Encore

---
+++ app/Resources/assets/js/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import '../css/app.sass';
+
+import $ from 'jquery';
+import 'bootstrap';
+import '@fortawesome/fontawesome-free/js/all'
+
+$(document).ready(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
