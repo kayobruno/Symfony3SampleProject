76f99b3322fad19cc874b651721cff86d05a4b18
Bruno Lima <bruno@gsd.com.br>
2019-09-25 10:57:36 -0300

    Remover Bootstrap
    Remover todo uso do Bootstrap e suas dependências.

--- app/Resources/assets/js/app.js
+++ app/Resources/assets/js/app.js
@@ -1,10 +1,1 @@
-
 import '../css/app.sass'
-
-import $ from 'jquery'
-import 'bootstrap'
-import '@fortawesome/fontawesome-free/js/all'
-
-$(document).ready(() => {
-  $('[data-toggle="tooltip"]').tooltip()
-})
